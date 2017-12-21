97e812f3c63df74049ea3f59196dc9a2c73d0670
Sam Dahlberg <dahlbergs@wit.edu>
2017-12-20 22:08:16 -0500

    Initial Commit

---
+++ init.el
@@ -0,0 +1,26 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(org-babel-load-file "~/.emacs.d/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("9f569b5e066dd6ca90b3578ff46659bc09a8764e81adf6265626d7dc0fac2a64" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(package-selected-packages
+   (quote
+    (cmake-mode ggtags htmlize spaceline counsel-gtags treemacs powerline solarized-theme undo-tree evil atom-one-dark-theme unbound multi-term counsel-projectile projectile yasnippet web-mode use-package org-bullets neotree matlab-mode highlight-parentheses flycheck-google-cpplint doom-themes counsel auto-complete))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-lock-comment-face ((t (:foreground "#1BA218"))))
+ '(fringe ((t (:inherit default :foreground "#8f949a")))))
